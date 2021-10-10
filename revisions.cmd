d1d1da4c2eeaafdd8c470a3108b0faa2e1574355
Conway <yawnoc@pm.me>
2021-10-10 14:02:48 +0800

    Add response to Examiner 1 'extend'

--- revisions.cmd
+++ revisions.cmd
@@ -8,6 +8,9 @@
 %css a~~~~~~~~
   :root {
     --solid-line: 1px solid black;
+    --solid-line-thick: 3px solid black;
+    --line-grey: #949494;
+    --text-grey: #595959;
   }
   @page {
     border-top: var(--solid-line);
@@ -24,6 +27,12 @@
       margin-top: 0.5em;
       vertical-align: top;
     }
+  }
+  blockquote {
+    border-left: var(--solid-line-thick);
+    margin-left: 0.5em;
+    margin-bottom: 0;
+    padding: 0.3em 0.5em;
   }
   body {
     font-family: Tinos, serif;
@@ -43,12 +52,30 @@
   }
   h3 {
     font-size: 1.2em;
+    margin-top: 1.3em;
+  }
+  ol {
+    margin: 0.3em 0;
+    padding-left: 1.7em;
+  }
+  .action {
+    border: var(--solid-line);
+    margin-right: 0.15em;
+    padding: 0.05em 0.2em;
+  }
+  .examiner {
+    color: var(--text-grey);
+    border-color: var(--line-grey);
+    font-family: Arimo, sans-serif;
   }
 ~~~~~~~~
 %%%%%%%%%%%%%%%%
 
 {: \h3-exam : Suggestions from Examiner's Report :}
 {: \h3-defence : Suggestions from Viva Voce :}
+
+{: [NO] : <span class="action">Not adopted.</span> :}
+{: [YES] : <span class="action">Adopted.</span> :}
 
 
 # Ph.D. Thesis Revisions #
@@ -58,6 +85,20 @@
 
 
 ### \h3-exam ###
+
+++++++++++++++++
+1.
+  """"{.examiner}
+  If I were critical I would ask that the candidate
+  extend both Section~1.1 and Chapter~9.
+  [...]
+  However I am not insistent that these modification[s] are made;
+  they are certainly not a deal-breaker by any stretch.
+  """"
+  ----
+  [NO] This suggestion was optional.
+  ----
+++++++++++++++++
 
 
 ### \h3-defence ###
