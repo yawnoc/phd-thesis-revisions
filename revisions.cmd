23a5f3b95a5d161db2658542c3a11bcd68cd41f4
Conway <yawnoc@pm.me>
2021-10-10 13:22:19 +0800

    Create main document

---
+++ revisions.cmd
@@ -0,0 +1,33 @@
+%%%%%%%%%%%%%%%%
+%title
+  revisions
+
+%resources
+  <script src="paged.polyfill.js"></script>
+
+%css a~~~~~~~~
+  :root {
+    --solid-line: 1px solid black;
+  }
+  @page {
+    border-top: var(--solid-line);
+    margin: 2cm;
+    padding-top: 0.5em;
+    size: A4;
+    @top-left {
+      content: "Conway Li";
+      margin-bottom: 0.3em;
+      vertical-align: bottom;
+    }
+    @bottom-right {
+      content: "Page " counter(page) " of " counter(pages);
+      margin-top: 0.5em;
+      vertical-align: top;
+    }
+  }
+  body {
+    font-family: Tinos, serif;
+    font-size: 11pt;
+  }
+~~~~~~~~
+%%%%%%%%%%%%%%%%
