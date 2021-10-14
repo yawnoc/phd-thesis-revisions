a754d6c158f8c84480641ddfb99a3bb9cda39f82
Conway <yawnoc@pm.me>
2021-10-15 00:27:31 +0800

    Add response to Examiner 2 [Page 54] 'phi small'

--- revisions.cmd
+++ revisions.cmd
@@ -608,6 +608,15 @@ and I note that I had no control over how the request for it was worded.
   Figure~4.7 (directly above) also makes it clear
   that the tangency occurs at~{{ϕ}} = 0.
   ----
+
+1.
+  """"{.examiner}
+  [Page~54, equation~(4.37)]
+  clarifying you're expanding phi small here.
+  """"
+  ----
+  [NO] Obvious from the context.
+  ----
 ++++++++++++++++
 
 
